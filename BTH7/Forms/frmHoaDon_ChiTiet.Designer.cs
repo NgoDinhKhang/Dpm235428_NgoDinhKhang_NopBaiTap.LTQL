a0c3fc670321d779b811d8a5edc2e8ae5153c895
NgoDinhKhang <khang_dpm235428@student.agu.edu.vn>
2026-04-16 16:03:07 +0700

    Add files via upload

---
+++ BTH7/Forms/frmHoaDon_ChiTiet.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace BTH1.Forms
+{
+    partial class frmHoaDon_ChiTiet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            txtGhiChuHoaDon = new TextBox();
+            cboKhachHang = new ComboBox();
+            cboNhanVien = new ComboBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            numDonGiaBan = new NumericUpDown();
+            dataGridView = new DataGridView();
+            ID = new DataGridViewTextBoxColumn();
+            TenSanPham = new DataGridViewTextBoxColumn();
+            DonGiaBan = new DataGridViewTextBoxColumn();
+            SoLuongBan = new DataGridViewTextBoxColumn();
+            ThanhTien = new DataGridViewTextBoxColumn();
+            btnXoa = new Button();
+            btnXacNhanBan = new Button();
+            numSoLuongBan = new NumericUpDown();
+            label6 = new Label();
+            label5 = new Label();
+            cboSanPham = new ComboBox();
+            label4 = new Label();
+            btnLuuHoaDon = new Button();
+            btnInHoaDon = new Button();
+            btnThoat = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numDonGiaBan).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numSoLuongBan).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(txtGhiChuHoaDon);
+            groupBox1.Controls.Add(cboKhachHang);
+            groupBox1.Controls.Add(cboNhanVien);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 100);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin hóa đơn";
+            // 
+            // txtGhiChuHoaDon
+            // 
+            txtGhiChuHoaDon.Location = new Point(153, 47);
+            txtGhiChuHoaDon.Name = "txtGhiChuHoaDon";
+            txtGhiChuHoaDon.Size = new Size(460, 23);
+            txtGhiChuHoaDon.TabIndex = 6;
+            // 
+            // cboKhachHang
+            // 
+            cboKhachHang.FormattingEnabled = true;
+            cboKhachHang.Location = new Point(492, 18);
+            cboKhachHang.Name = "cboKhachHang";
+            cboKhachHang.Size = new Size(121, 23);
+            cboKhachHang.TabIndex = 5;
+            // 
+            // cboNhanVien
+            // 
+            cboNhanVien.FormattingEnabled = true;
+            cboNhanVien.Location = new Point(151, 18);
+            cboNhanVien.Name = "cboNhanVien";
+            cboNhanVien.Size = new Size(243, 23);
+            cboNhanVien.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(400, 21);
+            label3.Name = "label3";
+            label3.Size = new Size(86, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Khách hàng(*):";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(49, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Ghi chú hóa đơn:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nhân viên lập(*):";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(numDonGiaBan);
+            groupBox2.Controls.Add(dataGridView);
+            groupBox2.Controls.Add(btnXoa);
+            groupBox2.Controls.Add(btnXacNhanBan);
+            groupBox2.Controls.Add(numSoLuongBan);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(cboSanPham);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Location = new Point(12, 118);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 244);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Thông tin chi tiết hóa đơn";
+            // 
+            // numDonGiaBan
+            // 
+            numDonGiaBan.Location = new Point(265, 54);
+            numDonGiaBan.Maximum = new decimal(new int[] { 1000000000, 0, 0, 0 });
+            numDonGiaBan.Name = "numDonGiaBan";
+            numDonGiaBan.Size = new Size(120, 23);
+            numDonGiaBan.TabIndex = 8;
+            // 
+            // dataGridView
+            // 
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.AllowUserToDeleteRows = false;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Columns.AddRange(new DataGridViewColumn[] { ID, TenSanPham, DonGiaBan, SoLuongBan, ThanhTien });
+            dataGridView.Location = new Point(6, 88);
+            dataGridView.MultiSelect = false;
+            dataGridView.Name = "dataGridView";
+            dataGridView.Size = new Size(764, 150);
+            dataGridView.TabIndex = 7;
+            // 
+            // ID
+            // 
+            ID.HeaderText = "ID";
+            ID.Name = "ID";
+            // 
+            // TenSanPham
+            // 
+            TenSanPham.HeaderText = "Tên sản phẩm";
+            TenSanPham.Name = "TenSanPham";
+            // 
+            // DonGiaBan
+            // 
+            DonGiaBan.HeaderText = "Đơn giá bán";
+            DonGiaBan.Name = "DonGiaBan";
+            // 
+            // SoLuongBan
+            // 
+            SoLuongBan.HeaderText = "Số lượng bán";
+            SoLuongBan.Name = "SoLuongBan";
+            // 
+            // ThanhTien
+            // 
+            ThanhTien.HeaderText = "Thành tiền";
+            ThanhTien.Name = "ThanhTien";
+            // 
+            // btnXoa
+            // 
+            btnXoa.ForeColor = Color.Red;
+            btnXoa.Location = new Point(672, 55);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(75, 23);
+            btnXoa.TabIndex = 6;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnXacNhanBan
+            // 
+            btnXacNhanBan.Location = new Point(572, 55);
+            btnXacNhanBan.Name = "btnXacNhanBan";
+            btnXacNhanBan.Size = new Size(94, 23);
+            btnXacNhanBan.TabIndex = 6;
+            btnXacNhanBan.Text = "Xác nhận bán";
+            btnXacNhanBan.UseVisualStyleBackColor = true;
+            btnXacNhanBan.Click += btnXacNhanBan_Click;
+            // 
+            // numSoLuongBan
+            // 
+            numSoLuongBan.Location = new Point(436, 54);
+            numSoLuongBan.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            numSoLuongBan.Name = "numSoLuongBan";
+            numSoLuongBan.Size = new Size(120, 23);
+            numSoLuongBan.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(436, 36);
+            label6.Name = "label6";
+            label6.Size = new Size(93, 15);
+            label6.TabIndex = 4;
+            label6.Text = "Số lượng bán(*):";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(265, 36);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Đơn giá bán(*):";
+            // 
+            // cboSanPham
+            // 
+            cboSanPham.FormattingEnabled = true;
+            cboSanPham.Location = new Point(37, 54);
+            cboSanPham.Name = "cboSanPham";
+            cboSanPham.Size = new Size(189, 23);
+            cboSanPham.TabIndex = 1;
+            cboSanPham.SelectedIndexChanged += cboSanPham_SelectedIndexChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(37, 36);
+            label4.Name = "label4";
+            label4.Size = new Size(76, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Sản phẩm(*):";
+            // 
+            // btnLuuHoaDon
+            // 
+            btnLuuHoaDon.Location = new Point(220, 368);
+            btnLuuHoaDon.Name = "btnLuuHoaDon";
+            btnLuuHoaDon.Size = new Size(105, 23);
+            btnLuuHoaDon.TabIndex = 1;
+            btnLuuHoaDon.Text = "Lưu hóa đơn";
+            btnLuuHoaDon.UseVisualStyleBackColor = true;
+            btnLuuHoaDon.Click += btnLuuHoaDon_Click;
+            // 
+            // btnInHoaDon
+            // 
+            btnInHoaDon.Location = new Point(331, 368);
+            btnInHoaDon.Name = "btnInHoaDon";
+            btnInHoaDon.Size = new Size(92, 23);
+            btnInHoaDon.TabIndex = 1;
+            btnInHoaDon.Text = "In hóa đơn...";
+            btnInHoaDon.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            btnThoat.Location = new Point(429, 368);
+            btnThoat.Name = "btnThoat";
+            btnThoat.Size = new Size(75, 23);
+            btnThoat.TabIndex = 1;
+            btnThoat.Text = "Thoát";
+            btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // frmHoaDon_ChiTiet
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnThoat);
+            Controls.Add(btnInHoaDon);
+            Controls.Add(btnLuuHoaDon);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "frmHoaDon_ChiTiet";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Hóa đơn chi tiết";
+            Load += frmHoaDon_ChiTiet_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numDonGiaBan).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numSoLuongBan).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+        private NumericUpDown numericUpDown1;
+        private Label label5;
+        private ComboBox cboSanPham;
+        private Label label4;
+        private Button btnXoa;
+        private Button btnXacNhanBan;
+        private NumericUpDown numSoLuongBan;
+        private Label label6;
+        private DataGridView dataGridView;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn TenSanPham;
+        private DataGridViewTextBoxColumn DonGiaBan;
+        private DataGridViewTextBoxColumn SoLuongBan;
+        private DataGridViewTextBoxColumn ThanhTien;
+        private Button btnLuuHoaDon;
+        private Button btnInHoaDon;
+        private Button btnThoat;
+        private ComboBox cboNhanVien;
+        private TextBox txtGhiChuHoaDon;
+        private ComboBox cboKhachHang;
+        private NumericUpDown numDonGiaBan;
+    }
+}
---
+++ BTH7/Forms/frmMain.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace BTH1.Forms
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            statusStrip = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            lblTrangThai = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            lblLienKet = new ToolStripStatusLabel();
+            mnuHeThong = new ToolStripMenuItem();
+            mnuDangNhap = new ToolStripMenuItem();
+            mnuDangXuat = new ToolStripMenuItem();
+            mnuDoiMatKhau = new ToolStripMenuItem();
+            mnuThoat = new ToolStripMenuItem();
+            mnuQuanLy = new ToolStripMenuItem();
+            mnuLoaiSanPham = new ToolStripMenuItem();
+            mnuHangSanXuat = new ToolStripMenuItem();
+            mnuSanPham = new ToolStripMenuItem();
+            mnuKhachHang = new ToolStripMenuItem();
+            mnuNhanVien = new ToolStripMenuItem();
+            mnuHoaDon = new ToolStripMenuItem();
+            mnuBaoCaoThongKe = new ToolStripMenuItem();
+            mnuThongKeSanPham = new ToolStripMenuItem();
+            mnuThongKeDoanhThu = new ToolStripMenuItem();
+            mnuTroGiup = new ToolStripMenuItem();
+            mnuHuongDanSuDung = new ToolStripMenuItem();
+            mnuThongTinPhanMem = new ToolStripMenuItem();
+            menuStrip = new MenuStrip();
+            statusStrip.SuspendLayout();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            statusStrip.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, lblTrangThai, toolStripStatusLabel2, lblLienKet });
+            statusStrip.Location = new Point(0, 428);
+            statusStrip.Name = "statusStrip";
+            statusStrip.Size = new Size(800, 22);
+            statusStrip.TabIndex = 2;
+            statusStrip.Text = "statusStrip1";
+            statusStrip.ItemClicked += statusStrip_ItemClicked;
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(0, 17);
+            // 
+            // lblTrangThai
+            // 
+            lblTrangThai.Name = "lblTrangThai";
+            lblTrangThai.Size = new Size(95, 17);
+            lblTrangThai.Text = "Chưa đăng nhập";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(627, 17);
+            toolStripStatusLabel2.Spring = true;
+            // 
+            // lblLienKet
+            // 
+            lblLienKet.IsLink = true;
+            lblLienKet.Name = "lblLienKet";
+            lblLienKet.Size = new Size(63, 17);
+            lblLienKet.Text = "© 2024 FIT";
+            lblLienKet.Click += lblLienKet_Click;
+            // 
+            // mnuHeThong
+            // 
+            mnuHeThong.DropDownItems.AddRange(new ToolStripItem[] { mnuDangNhap, mnuDangXuat, mnuDoiMatKhau, mnuThoat });
+            mnuHeThong.Name = "mnuHeThong";
+            mnuHeThong.Size = new Size(69, 20);
+            mnuHeThong.Text = "Hệ thống";
+            // 
+            // mnuDangNhap
+            // 
+            mnuDangNhap.Name = "mnuDangNhap";
+            mnuDangNhap.Size = new Size(180, 22);
+            mnuDangNhap.Text = "Đăng nhập ";
+            mnuDangNhap.Click += mnuDangNhap_Click;
+            // 
+            // mnuDangXuat
+            // 
+            mnuDangXuat.Name = "mnuDangXuat";
+            mnuDangXuat.Size = new Size(180, 22);
+            mnuDangXuat.Text = "Đăng xuất";
+            mnuDangXuat.Click += mnuDangXuat_Click;
+            // 
+            // mnuDoiMatKhau
+            // 
+            mnuDoiMatKhau.Name = "mnuDoiMatKhau";
+            mnuDoiMatKhau.Size = new Size(180, 22);
+            mnuDoiMatKhau.Text = "Đổi mật khẩu";
+            // 
+            // mnuThoat
+            // 
+            mnuThoat.Name = "mnuThoat";
+            mnuThoat.Size = new Size(180, 22);
+            mnuThoat.Text = "Thoát";
+            // 
+            // mnuQuanLy
+            // 
+            mnuQuanLy.DropDownItems.AddRange(new ToolStripItem[] { mnuLoaiSanPham, mnuHangSanXuat, mnuSanPham, mnuKhachHang, mnuNhanVien, mnuHoaDon });
+            mnuQuanLy.Name = "mnuQuanLy";
+            mnuQuanLy.Size = new Size(60, 20);
+            mnuQuanLy.Text = "Quản lý";
+            // 
+            // mnuLoaiSanPham
+            // 
+            mnuLoaiSanPham.Name = "mnuLoaiSanPham";
+            mnuLoaiSanPham.Size = new Size(173, 22);
+            mnuLoaiSanPham.Text = "Loại sản phẩm";
+            mnuLoaiSanPham.Click += mnuLoaiSanPham_Click;
+            // 
+            // mnuHangSanXuat
+            // 
+            mnuHangSanXuat.Name = "mnuHangSanXuat";
+            mnuHangSanXuat.Size = new Size(173, 22);
+            mnuHangSanXuat.Text = "Hãng sản xuất";
+            mnuHangSanXuat.Click += mnuHangSanXuat_Click;
+            // 
+            // mnuSanPham
+            // 
+            mnuSanPham.Name = "mnuSanPham";
+            mnuSanPham.Size = new Size(173, 22);
+            mnuSanPham.Text = "Sản phẩm";
+            mnuSanPham.Click += mnuSanPham_Click;
+            // 
+            // mnuKhachHang
+            // 
+            mnuKhachHang.Name = "mnuKhachHang";
+            mnuKhachHang.Size = new Size(173, 22);
+            mnuKhachHang.Text = "Khách hàng";
+            mnuKhachHang.Click += mnuKhachHang_Click;
+            // 
+            // mnuNhanVien
+            // 
+            mnuNhanVien.Name = "mnuNhanVien";
+            mnuNhanVien.Size = new Size(173, 22);
+            mnuNhanVien.Text = "Nhân viên";
+            mnuNhanVien.Click += mnuNhanVien_Click;
+            // 
+            // mnuHoaDon
+            // 
+            mnuHoaDon.Name = "mnuHoaDon";
+            mnuHoaDon.Size = new Size(173, 22);
+            mnuHoaDon.Text = "Hóa đơn bán hàng";
+            mnuHoaDon.Click += mnuHoaDon_Click;
+            // 
+            // mnuBaoCaoThongKe
+            // 
+            mnuBaoCaoThongKe.DropDownItems.AddRange(new ToolStripItem[] { mnuThongKeSanPham, mnuThongKeDoanhThu });
+            mnuBaoCaoThongKe.Name = "mnuBaoCaoThongKe";
+            mnuBaoCaoThongKe.Size = new Size(119, 20);
+            mnuBaoCaoThongKe.Text = "Báo cáo - thống kê";
+            // 
+            // mnuThongKeSanPham
+            // 
+            mnuThongKeSanPham.Name = "mnuThongKeSanPham";
+            mnuThongKeSanPham.Size = new Size(181, 22);
+            mnuThongKeSanPham.Text = "Thống kê sản phẩm";
+            // 
+            // mnuThongKeDoanhThu
+            // 
+            mnuThongKeDoanhThu.Name = "mnuThongKeDoanhThu";
+            mnuThongKeDoanhThu.Size = new Size(181, 22);
+            mnuThongKeDoanhThu.Text = "Thống kê doanh thu";
+            // 
+            // mnuTroGiup
+            // 
+            mnuTroGiup.DropDownItems.AddRange(new ToolStripItem[] { mnuHuongDanSuDung, mnuThongTinPhanMem });
+            mnuTroGiup.Name = "mnuTroGiup";
+            mnuTroGiup.Size = new Size(62, 20);
+            mnuTroGiup.Text = "Trợ giúp";
+            // 
+            // mnuHuongDanSuDung
+            // 
+            mnuHuongDanSuDung.Name = "mnuHuongDanSuDung";
+            mnuHuongDanSuDung.Size = new Size(186, 22);
+            mnuHuongDanSuDung.Text = "Hướng dẫn sử dụng";
+            // 
+            // mnuThongTinPhanMem
+            // 
+            mnuThongTinPhanMem.Name = "mnuThongTinPhanMem";
+            mnuThongTinPhanMem.Size = new Size(186, 22);
+            mnuThongTinPhanMem.Text = "Thông tin phần mềm";
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { mnuHeThong, mnuQuanLy, mnuBaoCaoThongKe, mnuTroGiup });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(800, 24);
+            menuStrip.TabIndex = 0;
+            menuStrip.Text = "menuStrip1";
+            menuStrip.ItemClicked += menuStrip_ItemClicked;
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip);
+            Controls.Add(menuStrip);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip;
+            Name = "frmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Quản lý bán hàng";
+            WindowState = FormWindowState.Maximized;
+            Load += frmMain_Load;
+            statusStrip.ResumeLayout(false);
+            statusStrip.PerformLayout();
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private StatusStrip statusStrip;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel lblTrangThai;
+        private ToolStripMenuItem mnuHeThong;
+        private ToolStripMenuItem mnuDangNhap;
+        private ToolStripMenuItem mnuDangXuat;
+        private ToolStripMenuItem mnuDoiMatKhau;
+        private ToolStripMenuItem mnuThoat;
+        private ToolStripMenuItem mnuQuanLy;
+        private ToolStripMenuItem mnuLoaiSanPham;
+        private ToolStripMenuItem mnuHangSanXuat;
+        private ToolStripMenuItem mnuSanPham;
+        private ToolStripMenuItem mnuKhachHang;
+        private ToolStripMenuItem mnuNhanVien;
+        private ToolStripMenuItem mnuBaoCaoThongKe;
+        private ToolStripMenuItem mnuThongKeSanPham;
+        private ToolStripMenuItem mnuThongKeDoanhThu;
+        private ToolStripMenuItem mnuTroGiup;
+        private ToolStripMenuItem mnuHuongDanSuDung;
+        private ToolStripMenuItem mnuThongTinPhanMem;
+        private MenuStrip menuStrip;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+        private ToolStripStatusLabel lblLienKet;
+        private ToolStripMenuItem mnuHoaDon;
+    }
+}
